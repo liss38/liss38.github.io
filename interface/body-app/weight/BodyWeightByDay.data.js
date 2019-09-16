2867c5de29fb8edf07c893e80d4571fb10fa6a55
Max Salnikov <liss38@yandex.ru>
2019-09-16 13:51:43 +0300

    BodyApp[weight]: продумать и создать структуру для массы по дням; продумать и осздать структуру для массы по неделям; заполнить таблицу массы по дням;

--- interface/body-app/weight/BodyWeightByDay.data.js
+++ interface/body-app/weight/BodyWeightByDay.data.js
@@ -1,1 +1,86 @@
-const BodyWeightByDay = [];
+const BodyWeightByDay = [
+	// @ { dateText: '', value: ., }, 
+
+	// #29
+	{ dateText: '15/07/2019', value: 72.8, }, 
+	{ dateText: '16/07/2019', value: 73.5, }, 
+	{ dateText: '17/07/2019', value: 72.7, }, 
+	{ dateText: '18/07/2019', value: 72.7, }, 
+	{ dateText: '19/07/2019', value: 72.6, }, 
+	{ dateText: '20/07/2019', value: 73.0, }, 
+	{ dateText: '21/07/2019', value: 72.5, }, 
+
+	// #30
+	{ dateText: '22/07/2019', value: 73.2, }, 
+	{ dateText: '23/07/2019', value: 72.8, }, 
+	{ dateText: '24/07/2019', value: 73.1, }, 
+	{ dateText: '25/07/2019', value: 73.0, }, 
+	{ dateText: '26/07/2019', value: 73.2, }, 
+	{ dateText: '27/07/2019', value: 73.0, }, 
+	{ dateText: '28/07/2019', value: 73.2, }, 
+
+	// #31
+	{ dateText: '29/07/2019', value: 73.2, }, 
+	{ dateText: '30/07/2019', value: 73.2, }, 
+	{ dateText: '31/07/2019', value: 73.5, }, 
+	{ dateText: '01/08/2019', value: 72.8, }, 
+	{ dateText: '02/08/2019', value: 74.5, }, 
+	{ dateText: '03/08/2019', value: 74.5, }, 
+	{ dateText: '04/08/2019', value: 73.8, }, 
+
+	// #32
+	{ dateText: '05/08/2019', value: 73.6, }, 
+	{ dateText: '06/08/2019', value: 73.4, }, 
+	{ dateText: '07/08/2019', value: 74.1, }, 
+	{ dateText: '08/08/2019', value: 73.6, }, 
+	{ dateText: '09/08/2019', value: 73.6, }, 
+	{ dateText: '10/08/2019', value: 73.2, }, 
+	{ dateText: '11/08/2019', value: 73.9, }, 
+
+	// #33
+	{ dateText: '12/08/2019', value: 73.3, }, 
+	{ dateText: '13/08/2019', value: 73.0, }, 
+	{ dateText: '14/08/2019', value: 73.7, }, 
+	{ dateText: '15/08/2019', value: 73.3, }, 
+	{ dateText: '16/08/2019', value: 73.7, }, 
+	{ dateText: '17/08/2019', value: 73.7, }, 
+	{ dateText: '18/08/2019', value: 74.5, }, 
+
+	// #34
+	{ dateText: '19/08/2019', value: 73.8, }, 
+	{ dateText: '20/08/2019', value: 73.3, }, 
+	{ dateText: '21/08/2019', value: 73.3, }, 
+	{ dateText: '22/08/2019', value: 72.4, }, 
+	{ dateText: '23/08/2019', value: 72.3, }, 
+	{ dateText: '24/08/2019', value: 72.9, }, 
+	{ dateText: '25/08/2019', value: 72.6, }, 
+
+	// #35
+	{ dateText: '26/08/2019', value: 73.0, }, 
+	{ dateText: '27/08/2019', value: 72.6, }, 
+	{ dateText: '28/08/2019', value: 72.4, }, 
+	{ dateText: '29/08/2019', value: 71.9, }, 
+	{ dateText: '30/08/2019', value: 71.9, }, 
+	{ dateText: '31/08/2019', value: 71.7, }, 
+	{ dateText: '01/09/2019', value: 72.4, }, 
+
+	// #36
+	{ dateText: '02/09/2019', value: 72.5, }, 
+	{ dateText: '03/09/2019', value: 71.8, }, 
+	{ dateText: '04/09/2019', value: 71.9, }, 
+	{ dateText: '05/09/2019', value: 71.6, }, 
+	{ dateText: '06/09/2019', value: 71.7, }, 
+	{ dateText: '07/09/2019', value: 71.8, }, 
+	{ dateText: '08/09/2019', value: 71.8, }, 
+
+	// #37
+	{ dateText: '09/09/2019', value: 71.9, }, 
+	{ dateText: '10/09/2019', value: 72.3, }, 
+	{ dateText: '11/09/2019', value: 72.4, }, 
+	{ dateText: '12/09/2019', value: 71.9, }, 
+	{ dateText: '13/09/2019', value: 71.9, }, 
+	{ dateText: '14/09/2019', value: 72.0, }, 
+	{ dateText: '15/09/2019', value: 71.6, }, 
+
+	// @ { dateText: '', value: ., }, 
+];
--- interface/body-app/weight/BodyWeightByWeek.data.js
+++ interface/body-app/weight/BodyWeightByWeek.data.js
@@ -1,1 +1,4 @@
-const BodyWeightByWeek = [];
+const BodyWeightByWeek = [
+	// @ { weekNumber: 23, min: 71.7, max: 72.9, average: 72.0, }, 
+	// @ { weekNumber: 23, min: 71.7, max: 72.9, average: 72.0, }, 
+];
