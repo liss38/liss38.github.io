5dee94666bf0d210a9cbebc6b661a2c2ee02737f
Max Salnikov <liss38@yandex.ru>
2020-02-13 18:22:33 +0300

    stuff-1: simple-app, закинул билд

---
+++ stuff/1/precache-manifest.859d8652172caea7232b62914f8ac46f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19397626b5e3f6b79528f533ebea80e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d53160d6db126e13f61",
+    "url": "/static/js/2.519c9a58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.519c9a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001b9e4ec3cf87417db9",
+    "url": "/static/js/main.b974f526.chunk.js"
+  },
+  {
+    "revision": "945117d77c66d08ddc36",
+    "url": "/static/js/runtime-main.e47ca86d.js"
+  }
+]);
